b94a9acee8a7bf6065aefb3d7b928e1cb2a57f88
Yunus Guneyli <yunusguneyli@Yunus-MacBook-Pro.local>
2025-12-11 02:29:13 +0300

    chore: Removed temporary content sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,13 @@ import Footer from './components/Footer';
 
 export default function Home() {
   return (
-    <main className="min-h-screen"> 
+    <main> 
       
       <Navbar /> 
+      
       <HeroSlider /> 
-      <div className="h-screen bg-gray-500 flex items-center justify-center">
-        <h2 className="text-3xl text-white">Kaydırma Alanı (Test)</h2>
-      </div>
-      <div className="h-screen bg-gray-600 flex items-center justify-center">
-        <h2 className="text-3xl text-white">Daha Fazla İçerik</h2>
-      </div>
-      <Footer />
+      
+      <Footer /> 
       
     </main>
   );
